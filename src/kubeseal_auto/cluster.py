66485025fedba035e15b54cb8a9433dd55810688
Vadims Gedzs <vadims@linux-tech.io>
2022-05-06 23:44:52 +0300

    minor cleanup in cluster.py

--- src/kubeseal_auto/cluster.py
+++ src/kubeseal_auto/cluster.py
@@ -7,33 +7,29 @@ from kubernetes import client, config
 class Cluster:
     def __init__(self):
         config.load_kube_config()
-        self.context = config.list_kube_config_contexts()[1]["name"]
-        self.controller = self.find_sealed_secrets_controller()
+        self.controller = self._find_sealed_secrets_controller()
 
     @staticmethod
     def get_all_namespaces() -> list:
-        namespaces = [
-            ns.metadata.name for ns in client.CoreV1Api().list_namespace().items
-        ]
-        ic(namespaces)
-        return namespaces
+        ns_list = [ns.metadata.name for ns in client.CoreV1Api().list_namespace().items]
+        ic(ns_list)
+        return ns_list
 
     @staticmethod
-    def find_sealed_secrets_controller() -> dict:
+    def _find_sealed_secrets_controller() -> dict:
         click.echo("===> Searching for SealedSecrets controller")
 
+        # Here we are basically making an educated guess that deployment that contains
+        # "sealed" in name is the one we are looking for.
+        # It will be good to change this logic to something more reasonable in the future.
         for deployment in client.AppsV1Api().list_deployment_for_all_namespaces().items:
             if "sealed" in deployment.metadata.name:
                 name = deployment.metadata.labels["app.kubernetes.io/instance"]
                 namespace = deployment.metadata.namespace
                 click.echo(
-                    "===> Found the following controller: "
-                    f"{Fore.CYAN}{namespace}/{name}"
+                    f"===> Found the following controller: {Fore.CYAN}{namespace}/{name}"
                 )
-                return {
-                    "name": name,
-                    "namespace": namespace,
-                }
+                return {"name": name, "namespace": namespace}
 
     def get_controller_name(self):
         return self.controller["name"]
@@ -41,5 +37,6 @@ class Cluster:
     def get_controller_namespace(self):
         return self.controller["namespace"]
 
-    def get_context(self):
-        return self.context
+    @staticmethod
+    def get_context():
+        return config.list_kube_config_contexts()[1]["name"]
