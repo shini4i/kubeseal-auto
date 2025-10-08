29e6d75b6e761c919b7d1a67851e335fdec65a6b
Vadim Gedz <github@shini4i.dev>
2025-10-08 21:01:10 +0300

    add initial flake.nix

---
+++ flake.nix
@@ -0,0 +1,85 @@
+{
+  description = "An interactive wrapper for kubeseal binary";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        python = pkgs.python313;
+
+        pythonDependencies = with python.pkgs; [
+          pyyaml
+          requests
+          kubernetes
+          click
+          icecream
+          questionary
+          colorama
+        ];
+
+        checkDependencies = with python.pkgs; [
+          pytest
+          pytest-cov
+        ];
+        
+        allPythonPackages = pythonDependencies ++ checkDependencies;
+
+      in
+      {
+        packages = {
+          kubeseal-auto = python.pkgs.buildPythonApplication {
+            pname = "kubeseal-auto";
+            version = "0.6.0";
+            src = self;
+
+            nativeBuildInputs = [
+              python.pkgs.poetry-core
+            ];
+
+            propagatedBuildInputs = pythonDependencies ++ [
+              pkgs.kubectl
+            ];
+
+            checkInputs = checkDependencies;
+
+            doCheck = true;
+            checkPhase = ''
+              runHook preCheck
+              make test-coverage
+              runHook postCheck
+            '';
+
+            meta = with pkgs.lib; {
+              description = "An interactive wrapper for kubeseal binary";
+              homepage = "https://github.com/shini4i/kubeseal-auto";
+              license = licenses.mit;
+              maintainers = with maintainers; [ ];
+            };
+          };
+
+          default = self.packages.${system}.kubeseal-auto;
+        };
+
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            poetry
+            pre-commit
+
+            (python.withPackages (ps: allPythonPackages))
+          ];
+
+          buildInputs = with pkgs; [
+            kubectl
+          ];
+        };
+
+        homeManagerModules.default = {
+          home.packages = [ self.packages.${system}.kubeseal-auto ];
+        };
+      });
+}
